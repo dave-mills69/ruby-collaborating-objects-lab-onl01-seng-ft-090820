470a375f4d5f772a54bffb824dfd6d52cc0b3708
David Mills <david_mills109@yahoo.com>
2020-09-30 21:00:36 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,63 +1,46 @@
-require 'pry'
-
 class Artist
-  attr_accessor :name, :songs, :artist
-
-  @@all = []
+  attr_accessor :name, :songs
+  @@all = []  #stores all instances of Artist 
 
   def initialize(name)
     @name = name
-    @songs = []
-    @@all = []
+    @songs = [Song.all]
   end
 
   def add_song(song)
-    @songs << song
+    self.songs << song    #Artist.songs reader returns all songs 
+                          #stored in @songs
   end
 
   def save
-    @@all << self
+    @@all << self     #saves all created instances of Artist class in 
+                      #@@all class variable
   end
-#this method allows a user to store their artist information in an array to be called back later
-#puts the entire artist instance into the class method all, which includes ALL info for ALL artist objects
 
-  def self.all
+  def self.all      #class method all returns all instances of Artist class 
     @@all
   end
-  #Different than #save, this class method allows the user to return all info from a particular artist instance
 
-  # def self.find_or_create_by_name(name)
-  #   if self.find(name)
-  #     self.find(name)
-  #   else
-  #     self.create(name)
-  #   end
-  # end
-
-  def self.find_or_create_by_name(name)
-    self.find(name) ? self.find(name) : self.create(name)
-  end
-
-
-  #This method should utilize 2 different methods, one for #find and the other for #create, than set the conditions
-
-  def self.find(name)
-    self.all.find { |artist| artist.name == name }
-  end
-  #Custom method to match the name of the arist being passed in with the current instance we're in
-  #This makes sure that we're in the correct artists' instance
-
-  def self.create(name)
-    self.new(name).tap {|artist| artist.save}
-  end
-  #When creating a new object instance in the traditional way, the return value is equal to the last variable definition
-  #THIS example uses the #tap method, which allows a user to create a new instance of an object, defne its variables, and
-  #return the object itself rather than the last variable in the implicit #initialize
-
-  def print_songs
-    songs.each do |song|
-      puts song.name
+  def self.find_or_create_by_name(name)   #class method uses find/create class 
+                                          #methods to detect or create instances
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
     end
   end
 
+  def self.find(name)     #class method detects instances from @@all class variable
+    self.all.detect { |artist| artist.name == name }
+  end
+
+  def self.create(name)   #class method creates & stores instances vs initializing
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
+
+  def print_songs #instance method iterates through @songs of an instance of Artist
+    self.songs.each {|song| song.name}
+  end
 end
